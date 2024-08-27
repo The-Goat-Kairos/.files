215a327c3851dadc508524917e3f8477f0f3fabc
Kairos <115362651+The-Goat-Kairos@users.noreply.github.com>
2024-08-27 19:06:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt install git vim lua5.1 lua5.4 nodejs npm python3 ruby java ripgrep luarocks
+sudo apt install git vim lua5.1 lua5.4 nodejs npm python3 ruby ripgrep luarocks
 sudo apt add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
@@ -8,3 +8,4 @@ sudo apt-get install -y dotnet-sdk-7.0
 dotnet tool install -g git-credential-manager
 export PATH="$PATH:/home/kairos/.dotnet/tools"
 git-credential-manager configure
+gsettings set org.gnome.shell.extesions.dash-to-dock background-opacity 0.15
